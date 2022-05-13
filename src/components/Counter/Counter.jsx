2d7af721b8fa851be8bd370a5fd14370629c6aa6
joshua360x <jkw5499@psu.edu>
2022-05-13 18:03:20 -0400

    added intial reducer

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -8,11 +8,30 @@ const colors = {
 };
 
 export default function Counter() {
-  // const [state, dispatch] = useReducer(first, second, third)
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   const [currentColor, setCurrentColor] = useState(colors.yellow);
+  const intialCount = { count: 0 }
+  const [state, dispatch] = useReducer(reducer, intialCount)
+
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'increment':
+        return { count: state.count + 1 }
+      case 'decrement':
+        return { count: state.count - 1 }
+    
+      default:
+        throw new Error();
+    }
+  }
+
+
+
+
 
   useEffect(() => {
+    const { count } = intialCount
     if (count === 0) {
       setCurrentColor(colors.yellow);
     }
@@ -27,7 +46,8 @@ export default function Counter() {
   }, [count]);
 
   const increment = () => {
-    setCount((prevState) => prevState + 1);
+    // setCount((prevState) => prevState + 1);
+    dispatch({ type: 'increment' })
   };
 
   const decrement = () => {
@@ -40,6 +60,7 @@ export default function Counter() {
 
   return (
     <main className={styles.main}>
+      <h3>{state.count}</h3>
       <h1 style={{ color: currentColor }}>{count}</h1>
       <div>
         <button
